55a56df749a2ef5df035799e92b58992b7456cf2
michel novellino <michelnovellino.programador@gmail.com>
2020-02-07 20:14:43 -0400

    cambio

--- src/axios.js
+++ src/axios.js
@@ -6,14 +6,11 @@ const env = process.env.NODE_ENV === "development" ? dev : prod;
 var headers = {};
 const token = localStorage.getItem("bazam-token-control");
 
-/* var baseUrl =
+var baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:9090/v1"
-    : "http://api.cs100.cl/v1"; */
-    var baseUrl =
-    process.env.NODE_ENV === "development"
-      ? "http://api.cs100.cl/v1"
-      : "http://api.cs100.cl/v1";
+    : "http://api.cs100.cl/v1"; 
+
 if (token !== null && token !== undefined && token !== "") {
   headers["Authorization"] = "Bearer " + token;
 }
@@ -22,6 +19,8 @@ var instance = axios.create({
   baseURL: baseUrl,
   headers: headers
 });
+console.log(env.API_URL.toString())
+
 export const nodeInstance = axios.create({
   baseURL: env.API_URL.toString(),
   headers: headers
